421e96a9f6816103bbafe3ff67328f7049dd91af
baberarjumand <baber.arjumand@gmail.com>
2022-06-09 06:55:54 -0700

    setup basic pages and firebase creds

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,28 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { firebaseCredentials } from './firebase';
+
+// for firebase credentials, create a file named 'firebase.ts' in this folder.
+// Then copy and paste the following code into it, with your firebase credentials:
+
+// export const firebaseCredentials = {
+//   type: 'XXXXXXXXX',
+//   project_id: 'XXXXXXXXXXXXXXXXXX',
+//   private_key_id: 'XXXXXXXXXXXXXXXXXXXXXXXXXXX',
+//   private_key:
+//     '-----BEGIN PRIVATE KEY-----\nXXXXXXXXXXXXXXXXXXXXXXXXXX\n-----END PRIVATE KEY-----\n',
+//   client_email: 'XXXXXXXXX@XXXXXXXXXXXXXX.iam.gserviceaccount.com',
+//   client_id: 'XXXXXXXXXXXXXXXXXXXXXXX',
+//   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+//   token_uri: 'https://oauth2.googleapis.com/token',
+//   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+//   client_x509_cert_url: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+// };
+
 export const environment = {
-  production: false
+  production: false,
+  firebaseCreds: firebaseCredentials,
 };
 
 /*
